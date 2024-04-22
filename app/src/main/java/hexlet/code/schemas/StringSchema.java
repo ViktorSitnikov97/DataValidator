aa81a751d2c6e349388b01f0eddab70d81d34e80
Viktor Sitnikov <sitnikoff_victor_97@mail.ru>
2024-04-23 00:31:59 +0300

    update StringSchema.java and StringSchemaTest.java

--- app/src/main/java/hexlet/code/schemas/StringSchema.java
+++ app/src/main/java/hexlet/code/schemas/StringSchema.java
@@ -1,49 +1,26 @@
-package hexlet.code;
+package hexlet.code.schemas;
 
-import java.util.HashMap;
-import java.util.Map;
 import java.util.Objects;
 import java.util.function.Predicate;
 
-public final class StringSchema {
-
-    private boolean isInclude = false;
-    private final Object minLength = new Object();
-    private final Object contains = new Object();
-    private Map<Object, Predicate<Object>> map = new HashMap<>();
+public final class StringSchema extends BaseSchema<String> {
 
     public StringSchema required() {
-        isInclude = true;
+        Predicate<String> req = str -> !Objects.equals(str, null) && !str.isEmpty();
+        checkMap.put("required", req);
         return this;
     }
 
     public StringSchema minLength(int length) {
-        Predicate<Object> len = str -> str.toString().length() >= length;
-        map.put(minLength, len);
+        Predicate<String> len = str -> str.length() >= length;
+        checkMap.put("minLength", len);
         return this;
     }
 
     public StringSchema contains(String currentString) {
-        Predicate<Object> con = str -> str.toString().contains(currentString);
-        map.put(contains, con);
+        Predicate<String> con = str -> str.contains(currentString);
+        checkMap.put("contains", con);
         return this;
     }
 
-    public boolean isValid(String str) {
-        if (isInclude && (Objects.equals(str, null) || str.equals(""))) {
-            return false;
-        }
-        String string = str;
-
-        if (!map.isEmpty()) {
-            for (Map.Entry<Object, Predicate<Object>> pair : map.entrySet()) {
-                Predicate<Object> value = pair.getValue();
-                if (!value.test(string)) {
-                    return false;
-                }
-            }
-        }
-
-        return true;
-    }
 }
