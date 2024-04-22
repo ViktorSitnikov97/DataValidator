14fd6d9f45528623bce340b241a2c9d5c5786e18
Viktor Sitnikov <sitnikoff_victor_97@mail.ru>
2024-04-23 00:30:32 +0300

    update ValidatorTest.java

--- app/src/test/java/hexlet/code/ValidatorTest.java
+++ app/src/test/java/hexlet/code/ValidatorTest.java
@@ -1,6 +1,7 @@
 package hexlet.code;
 
 import hexlet.code.schemas.StringSchema;
+import hexlet.code.schemas.NumberSchema;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
@@ -10,6 +11,15 @@ public class ValidatorTest {
     public void stringTest() {
         Validator validator = new Validator();
         boolean actual = validator.string() instanceof StringSchema;
+        boolean expected = true;
+
+        Assertions.assertEquals(actual, expected);
+    }
+
+    @Test
+    public void numberTest() {
+        Validator validator = new Validator();
+        boolean actual = validator.number() instanceof NumberSchema;
         boolean expected = true;
 
         Assertions.assertEquals(actual, expected);
